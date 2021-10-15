fa90464ec4a70e379c154d137cf01bf50f04f9a8
Daniel van Ross <daniel.vanross@fiz-karlsruhe.de>
2021-10-16 00:07:46 +0200

    Setup initial project structure for MicroProfile application

---
+++ src/main/java/net/adoptium/documentationservices/DocumentationservicesRestApplication.java
@@ -0,0 +1,11 @@
+package net.adoptium.documentationservices;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ * JAX-RS application for documentation services.
+ */
+@ApplicationPath("/")
+public class DocumentationservicesRestApplication extends Application {
+}

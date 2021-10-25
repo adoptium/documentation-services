fe14a7236906b86836e283cceb1790b2dc9f177a
Hendrik Ebbers <hendrik.ebbers@web.de>
2021-10-25 20:35:34 +0200

    service for adoc conversion added

---
+++ src/main/java/net/adoptium/documentationservices/adoc/AsciiDocService.java
@@ -0,0 +1,52 @@
+package net.adoptium.documentationservices.adoc;
+
+import org.asciidoctor.Asciidoctor;
+import org.asciidoctor.Options;
+import org.asciidoctor.ast.Document;
+
+import javax.enterprise.context.ApplicationScoped;
+import java.nio.file.Path;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.Set;
+
+@ApplicationScoped
+public class AsciiDocService {
+
+    private final Asciidoctor asciidoctor;
+
+    public AsciiDocService() {
+        this.asciidoctor = Asciidoctor.Factory.create();
+    }
+
+    private Options createOptions() {
+        return Options.builder()
+                .headerFooter(false)
+                .build();
+    }
+
+    private Document loadDocument(final Path pathToAdoc) {
+        Objects.requireNonNull(pathToAdoc, "pathToAdoc should not be null!");
+        try {
+            return asciidoctor.loadFile(pathToAdoc.toFile(), createOptions());
+        } catch (final Exception exception) {
+            throw new AsciiDocException("Error in loading Asciidoc file '" + pathToAdoc + "'", exception);
+        }
+    }
+
+    public Map<String, String> getMetadata(final Path pathToAdoc) {
+        final Set<Map.Entry<String, Object>> entries = loadDocument(pathToAdoc).getAttributes()
+                .entrySet();
+        Map<String, String> metadata = new HashMap<>();
+        entries.forEach(entry -> metadata.put(entry.getKey(), Optional.ofNullable(entry.getValue()).map(v -> v.toString()).filter(v -> !v.isBlank()).orElse(null)));
+        return Collections.unmodifiableMap(metadata);
+    }
+
+    public String convert(final Path pathToAdoc) {
+        return loadDocument(pathToAdoc).convert();
+    }
+
+}
---
+++ src/test/java/net/adoptium/documentationservices/adoc/AsciiDocServiceTest.java
@@ -0,0 +1,83 @@
+package net.adoptium.documentationservices.adoc;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Map;
+
+class AsciiDocServiceTest {
+
+    @Test
+    public void testCreation() {
+        Assertions.assertDoesNotThrow(() -> new AsciiDocService());
+    }
+
+    @Test
+    public void testGetMetadataForNullPath() {
+        //given
+        final AsciiDocService service = new AsciiDocService();
+        final Path pathToAdoc = null;
+
+        //than
+        Assertions.assertThrows(NullPointerException.class, () -> service.getMetadata(pathToAdoc));
+    }
+
+    @Test
+    public void testGetMetadataForInvalidPath() throws URISyntaxException {
+        //given
+        final AsciiDocService service = new AsciiDocService();
+        final Path pathToAdoc = getLocalPath("invalid.adoc");
+
+        //than
+        Assertions.assertThrows(AsciiDocException.class, () -> service.getMetadata(pathToAdoc));
+    }
+
+    @Test
+    public void testConvertForInvalidPath() throws URISyntaxException {
+        //given
+        final AsciiDocService service = new AsciiDocService();
+        final Path pathToAdoc = getLocalPath("invalid.adoc");
+
+        //than
+        Assertions.assertThrows(AsciiDocException.class, () -> service.convert(pathToAdoc));
+    }
+
+    @Test
+    public void testGetMetadata() throws URISyntaxException {
+        //given
+        final AsciiDocService service = new AsciiDocService();
+        final Path pathToAdoc = getLocalPath("sample.adoc");
+
+        //when
+        final Map<String, String> metadata = service.getMetadata(pathToAdoc);
+
+        //than
+        Assertions.assertNotNull(metadata);
+        Assertions.assertTrue(metadata.containsKey("description"));
+        Assertions.assertTrue(metadata.containsKey("sectanchors"));
+        Assertions.assertEquals("A test document.", metadata.get("description"));
+        Assertions.assertEquals(null, metadata.get("sectanchors"));
+    }
+
+    @Test
+    public void testConvert() throws URISyntaxException, IOException {
+        //given
+        final AsciiDocService service = new AsciiDocService();
+        final Path pathToAdoc = getLocalPath("sample.adoc");
+        final String expectedContent = Files.readString(getLocalPath("sample-converted.txt"));
+
+        //when
+        final String content = service.convert(pathToAdoc);
+
+        Assertions.assertNotNull(content);
+        Assertions.assertEquals(expectedContent, content + System.lineSeparator()); //Auto Format adds newline to 'sample-converted.txt'
+    }
+
+    private Path getLocalPath(final String resourceName) {
+        return Path.of(AsciiDocServiceTest.class.getResource("").getPath(), resourceName);
+    }
+}

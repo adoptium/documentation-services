9455a56e4db5263b020656140ee2e88dcdf0bf89
Hendrik Ebbers <hendrik.ebbers@web.de>
2021-10-27 18:42:30 +0200

    getter & setter added

--- src/main/java/net/adoptium/documentationservices/api/LanguageInfo.java
+++ src/main/java/net/adoptium/documentationservices/api/LanguageInfo.java
@@ -14,4 +14,27 @@ public class LanguageInfo {
     @Schema(required = true, example = "de", description = "iso-XXX based 2 char language code of the language in that the name is translated")
     private String titleIsoCode;
 
+    public String getLanguageIsoCode() {
+        return languageIsoCode;
+    }
+
+    public void setLanguageIsoCode(final String languageIsoCode) {
+        this.languageIsoCode = languageIsoCode;
+    }
+
+    public String getTranslatedName() {
+        return translatedName;
+    }
+
+    public void setTranslatedName(final String translatedName) {
+        this.translatedName = translatedName;
+    }
+
+    public String getTitleIsoCode() {
+        return titleIsoCode;
+    }
+
+    public void setTitleIsoCode(final String titleIsoCode) {
+        this.titleIsoCode = titleIsoCode;
+    }
 }

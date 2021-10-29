22d6cbf6b2beb94b59859e9673172503dca2c742
vanross <daniel@vanross.de>
2021-10-29 10:19:13 +0200

    Basic implementation of monitoring repository and downloading repository content (#33)
    * Monitoring GitHub repo and downloading repo content to local filesystem
    * Fixed minor issues reported by static code analysis and corrected some typos.
    * Removed duplicate Microprofile dependency statement.
    * Added final modifiers where applicable and externalized strings.
    * Changed to constructor injection.
    * Switched logging to SLF4J

---
+++ src/main/java/net/adoptium/documentationservices/services/RepoService.java
@@ -0,0 +1,190 @@
+package net.adoptium.documentationservices.services;
+
+import org.kohsuke.github.*;
+import net.lingala.zip4j.ZipFile;
+import org.apache.commons.io.FileUtils;
+
+import javax.enterprise.context.ApplicationScoped;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.Proxy;
+import java.net.URL;
+import java.net.URLConnection;
+import java.nio.channels.Channels;
+import java.nio.channels.ReadableByteChannel;
+import java.nio.file.*;
+import java.time.Instant;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+import java.util.List;
+
+/**
+ * This service provides methods to retrieve data from GitHub using the GitHub API.
+ *
+ */
+@ApplicationScoped
+public class RepoService {
+
+    private static final String TARGET_DIRECTORY = "adoptium_files/";
+    private static final String TMP_DIRECTORY = "adoptium_files_tmp/";
+    private static final String TMP_FILE = "downloaded.zip";
+    private static final String METADATA_DIR = ".metadata";
+    private static final String LAST_UPDATE_FILE = "last_update";
+
+    private static final String ZIPBALL_SUFFIX = "/zipball";
+    private static final String TIMEZONE_NAME_FOR_SAVED_TIMESTAMP = "UTC";
+
+    private GHRepository repository = null;
+
+    // TODO - get the following from configuration
+    private String repositoryName = "adoptium/documentation";
+    private Proxy proxy = null;
+
+    /**
+     * Checks if the last update in the repository was after the saved timestamp.
+     *
+     * @return true if the last update is newer than the saved timestamp, false if not.
+     * @throws IOException if problems occurred accessing the local filesystem or requesting information from GitHub.
+     */
+    public boolean isUpdateAvailable() throws IOException {
+        final Path lastUpdateFile = getTimestampFile();
+        if (!Files.exists(lastUpdateFile)) {
+            return true;
+        }
+
+        Instant lastUpdateTimestamp = loadDateFromFile(lastUpdateFile);
+        Instant repoLastUpdated = getGitHubRepo().getUpdatedAt().toInstant();
+
+        return repoLastUpdated.isAfter(lastUpdateTimestamp);
+    }
+
+
+    /**
+     * Saves the given timestamp to be used for the next update-available-check.
+     *
+     * @param timestamp the timestamp to save
+     * @throws IOException if problems occurred accessing the local filesystem.
+     */
+    public void saveLastUpdateTimestamp(Instant timestamp) throws IOException {
+        saveDateToFile(getTimestampFile(), timestamp);
+    }
+
+
+    /**
+     * Downloads current main branch into local directory and returns directory reference.
+     *
+     * @return the path to the downloaded data
+     * @throws IOException if there were problems downloading or saving the data.
+     */
+    public Path downloadRepositoryContent() throws IOException {
+        final Path downloadedZipFile = downloadZipFile();
+
+        // unzip to temporary directory
+        final Path tempDirectory = getDataDir().resolve(TMP_DIRECTORY);
+        new ZipFile(downloadedZipFile.toString()).extractAll(tempDirectory.toString());
+
+        // delete existing copy
+        final Path targetDirectory = getDataDir().resolve(TARGET_DIRECTORY);
+        FileUtils.deleteDirectory(targetDirectory.toFile());
+
+        // rename temporary to target
+        Files.move(tempDirectory, targetDirectory, StandardCopyOption.ATOMIC_MOVE);
+
+        // delete downloaded zip
+        Files.delete(downloadedZipFile);
+
+        return targetDirectory;
+    }
+
+
+    /*
+     * Download ZIP of current repository main branch and returns filename of downloaded zip.
+     */
+    private Path downloadZipFile() throws IOException {
+        final Path downloadedZipFile = getDataDir().resolve(TMP_FILE);
+
+        final String archiveURL = getGitHubRepo().getUrl().toString() + ZIPBALL_SUFFIX;
+        final URL url = new URL(archiveURL);
+        final URLConnection connection;
+        if (proxy != null) {
+            connection = url.openConnection(proxy);
+        } else {
+            connection = url.openConnection();
+        }
+        try (InputStream urlInputStream = connection.getInputStream();
+             FileOutputStream fileOutputStream = new FileOutputStream(downloadedZipFile.toString(), false)) {
+            ReadableByteChannel urlInputChannel = Channels.newChannel(urlInputStream);
+            fileOutputStream.getChannel().transferFrom(urlInputChannel, 0, Long.MAX_VALUE);
+        }
+        return downloadedZipFile;
+    }
+
+    /*
+     * Saves given instant to file.
+     */
+    private void saveDateToFile(Path file, Instant timestamp) throws IOException {
+        if (!Files.exists(file.getParent())) {
+            Files.createDirectories(file.getParent());
+        }
+        final String timestampStr = DateTimeFormatter.ISO_OFFSET_DATE_TIME.withZone(ZoneId.of(TIMEZONE_NAME_FOR_SAVED_TIMESTAMP)).format(timestamp);
+        Files.writeString(file, timestampStr, StandardOpenOption.WRITE, StandardOpenOption.CREATE, StandardOpenOption.TRUNCATE_EXISTING);
+    }
+
+
+    /*
+     * Reads timestamp from file and returns value as Instant.
+     */
+    private Instant loadDateFromFile(Path file) throws IOException {
+        final List<String> lines = Files.readAllLines(file);
+        if (lines.isEmpty()) {
+            throw new IOException("Timestamp file was empty.");
+        }
+        String timestamp = lines.get(0);
+        try {
+            return Instant.from(DateTimeFormatter.ISO_OFFSET_DATE_TIME.withZone(ZoneId.of(TIMEZONE_NAME_FOR_SAVED_TIMESTAMP)).parse(timestamp));
+        } catch (DateTimeParseException e) {
+            throw new IOException("Timestamp file contained invalid data.");
+        }
+    }
+
+    /*
+     * Returns the directory used to store repository data locally.
+     */
+    private Path getDataDir() {
+        return Path.of(System.getProperty("jboss.server.data.dir"));
+    }
+
+
+    /*
+     * Returns the file to be used to save the timestamp of the last update.
+     */
+    private Path getTimestampFile() {
+        final Path metadataPath = getDataDir().resolve(METADATA_DIR);
+        return metadataPath.resolve(LAST_UPDATE_FILE);
+    }
+
+
+    /*
+     * Retrieves the repository instance to be used everywhere in this class.
+     */
+    private GHRepository getGitHubRepo() throws IOException {
+        if (repository == null) {
+            initRepo();
+        }
+        return repository;
+    }
+
+    /*
+     * Initializes GitHub connection and get repository instance.
+     */
+    private void initRepo() throws IOException {
+        final GitHubBuilder builder = new GitHubBuilder();
+        if (proxy != null) {
+            builder.withProxy(proxy);
+        }
+        final GitHub github = builder.build();
+        repository = github.getRepository(repositoryName);
+    }
+}
---
+++ src/test/java/net/adoptium/documentationservices/services/RepoServiceTest.java
@@ -0,0 +1,54 @@
+package net.adoptium.documentationservices.services;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.lang3.RandomStringUtils;
+import org.junit.jupiter.api.*;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.time.Instant;
+
+
+public class RepoServiceTest {
+
+    private static Path testDirectory;
+
+    @Test
+    public void testCreation() {
+        Assertions.assertDoesNotThrow(() -> new RepoService());
+    }
+
+    @BeforeAll
+    public static void setUpTempDirectory() {
+        testDirectory = Paths.get("/tmp/" + RandomStringUtils.randomAlphanumeric(8));
+        testDirectory.toFile().mkdirs();
+        System.setProperty("jboss.server.data.dir", testDirectory.toString());
+    }
+
+    @Test
+    public void testUpdateCycle() throws IOException {
+
+        final RepoService repoService = new RepoService();
+        boolean updateAvailable = repoService.isUpdateAvailable();
+        Assertions.assertTrue(updateAvailable);
+        
+        Path downloadedData = repoService.downloadRepositoryContent();
+        Assertions.assertTrue(Files.isDirectory(downloadedData));
+
+        Assertions.assertTrue(Files.list(downloadedData).findAny().isPresent());
+
+        repoService.saveLastUpdateTimestamp(Instant.now());
+
+        updateAvailable = repoService.isUpdateAvailable();
+
+        Assertions.assertFalse(updateAvailable);
+    }
+                
+    @AfterAll
+    public static void deleteTempDirectory() throws IOException {
+        FileUtils.deleteQuietly(testDirectory.toFile());
+    }
+
+}
